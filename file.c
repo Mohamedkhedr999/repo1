6b92a49668218219b6547abc87bed74624791617
Khedr <mohamedkhedr.mk39@gmail.com>
2023-07-15 18:34:34 +0200

    try

--- file.c
+++ file.c
@@ -5,3 +5,12 @@ u16 MPX4115_GetPressure(void)
 	u16 p = (((((u32)(adc - 55))*1000)) / (921)) + 150;
 	return p;
 }
+
+
+u16 MPX4115_GetPressure(void)
+{
+	u16 adc = ADC_Read(MPX4115_CH);
+	
+	u16 p = (((((u32)(adc - 55))*1000)) / (921)) + 150;
+	return p;
+}
